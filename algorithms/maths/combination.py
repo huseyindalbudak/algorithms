b75da870cce0fc9caa7c02620f9e7b62264a0746
huseyin dalbudak <93huseyin93@gmail.com>
2018-06-18 19:45:40 +0300

    optimization for the code
    the code so fast according to old code, such as combination(50,10)

--- algorithms/maths/combination.py
+++ algorithms/maths/combination.py
@@ -1,6 +1,17 @@
+'''
 def combination(n, r):
     # This function calculates nCr
     if n == r or r == 0:
         return 1
     else:
         return combination(n-1, r-1) + combination(n-1, r)
+'''
+ # the code so fast according to old code, such as combination(50,10)
+ def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+def combination(n,r):
+    return factorial(n) /factorial(r) / factorial(n-r)
